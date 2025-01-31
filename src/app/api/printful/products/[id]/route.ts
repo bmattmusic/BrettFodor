1aaf9aad87f2ef2a534bbcedefe21e0b162ea025
Brett Fodor <brett.fodor@gmail.com>
2025-01-31 15:18:35 -0500

    uninstall and reinstall next

--- src/app/api/printful/products/[id]/route.ts
+++ src/app/api/printful/products/[id]/route.ts
@@ -1,12 +1,16 @@
 import { type NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
+interface RouteSegment {
+  params: { id: string }
+}
+
 export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } }
+  _request: NextRequest,
+  { params }: RouteSegment
 ) {
   try {
-    const response = await fetch(`${process.env.PRINTFUL_API}/store/products/${context.params.id}`, {
+    const response = await fetch(`${process.env.PRINTFUL_API}/store/products/${params.id}`, {
       headers: {
         'Authorization': `Bearer ${process.env.PRINTFUL_TOKEN}`,
         'X-PF-Store-Id': `${process.env.PRINTFUL_STORE_ID}`
