9cd0c00364a25168978aa9bc57945387458d6786
Brett Fodor <brett.fodor@gmail.com>
2025-01-31 15:49:12 -0500

    installed builder

--- src/lib/builder.ts
+++ src/lib/builder.ts
@@ -1,7 +1,7 @@
-import { Builder } from '@builder.io/react'
+import { builder } from '@builder.io/react'
 import { env } from './env'
 
-Builder.init(env.BUILDER_API_KEY)
+builder.init(env.BUILDER_API_KEY)
 
 export const builderConfig = {
   apiKey: env.BUILDER_API_KEY,
