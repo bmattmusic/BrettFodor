ca9b15c7b4a8a441daca1ea20e5a580f82d07524
Brett Fodor <brett.fodor@gmail.com>
2025-01-31 15:52:52 -0500

    updated stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from 'stripe'
 import { env } from './env'
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
+  apiVersion: "2025-01-27.acacia",
   typescript: true,
 })
